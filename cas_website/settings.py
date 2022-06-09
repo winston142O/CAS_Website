9747ec4359564a35677982fbbaeca62c04e576c6
Winston Pichardo <winston1420@hotmail.com>
2022-06-09 07:08:53 -0500

    Update settings.py
    Secret Key

--- cas_website/settings.py
+++ cas_website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5vgg-_(7%_)vell$9__ccm8wx@#kq6e^*qd6duq2)%!k78h^ea'
+SECRET_KEY = '####################################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
